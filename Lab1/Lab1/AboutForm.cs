8211b2b284cc53dc5e4350b02a93985ac9fa7b45
trush <trushchakolga@gmail.com>
2023-09-14 22:46:06 +0300

    initial commit

---
+++ Lab1/Lab1/AboutForm.cs
@@ -0,0 +1,15 @@
+namespace Lab1
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
