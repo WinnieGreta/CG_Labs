6b39d3b57b627dc6050dc339fb0b079a3aeaba50
trush <trushchakolga@gmail.com>
2023-10-13 13:03:33 +0300

    lab2 finished

--- lab2/Form1.Designer.cs
+++ lab2/Form1.Designer.cs
@@ -62,6 +62,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "Triangle";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox2
             // 
--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -14,16 +14,8 @@ namespace lab2
     {
         Graphics g;
 
-        int x1 = 20;
-        int y1 = 20;
-        int x2 = 200;
-        int y2 = 20;
-        int x3 = 200;
-        int y3 = 200;
-        int x4 = 20;
-        int y4 = 200;
-
-        Bitmap mypic;
+        Bitmap mypic1;
+        Bitmap mypic2;
 
         public Form1()
         {
@@ -32,15 +24,24 @@ namespace lab2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int x1 = 20;
+            int y1 = 20;
+            int x2 = 200;
+            int y2 = 20;
+            int x3 = 200;
+            int y3 = 200;
+            int x4 = 20;
+            int y4 = 200;
+
             Point t1 = new Point(x1, y1);
             Point t2 = new Point(x2, y2);
             Point t3 = new Point(x3, y3);
             Point t4 = new Point(x4, y4);
 
-            mypic = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            pictureBox1.Image = mypic;
-            g = Graphics.FromImage(mypic);
-            g.DrawPolygon(new Pen(Color.BurlyWood, 2), new Point[] { t1, t2, t3, t4 });
+            mypic1 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = mypic1;
+            g = Graphics.FromImage(mypic1);
+            g.DrawPolygon(new Pen(Color.BurlyWood, 1), new Point[] { t1, t2, t3, t4 });
 
             float p = 0.08f;
 
@@ -69,18 +70,67 @@ namespace lab2
                 Point t3new = new Point(x3, y3);
                 Point t4new = new Point(x4, y4);
 
-                g.DrawPolygon(new Pen(Color.BurlyWood, 2), new Point[] { t1new, t2new, t3new, t4new });
+                g.DrawPolygon(new Pen(Color.BurlyWood, 1), new Point[] { t1new, t2new, t3new, t4new });
             }
-            
-            x1 = 20;
-            y1 = 20;
-            x2 = 200;
-            y2 = 20;
-            x3 = 200;
-            y3 = 200;
-            x4 = 20;
-            y4 = 200;
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pen mypen2 = new Pen(Color.Crimson, 1);
+
+            int x1 = 20;
+            int y1 = 20;
+            int x2 = 160;
+            int y2 = 150;
+            int x3 = 200;
+            int y3 = 50;
+
+            Point t1 = new Point(x1, y1);
+            Point t2 = new Point(x2, y2);
+            Point t3 = new Point(x3, y3);
+
+            mypic2 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            pictureBox2.Image = mypic2;
+            g = Graphics.FromImage(mypic2);
+            g.DrawPolygon(new Pen(Color.Crimson, 1), new Point[] { t1, t2, t3 });
+
+            float p = 0.5f;
+
+            DrawTriangles(x1, y1, x2, y2, x3, y3, mypen2, 5, p);
+
+        }
+
+        private void DrawTriangles(int x1, int y1, int x2, int y2, int x3, int y3, Pen mypen, int depth, float p)
+        {
+            if (depth != 0)
+            {
+                float x1new = x1 + (x2 - x1) * p;
+                float y1new = y1 + (y2 - y1) * p;
+                float x2new = x2 + (x3 - x2) * p;
+                float y2new = y2 + (y3 - y2) * p;
+                float x3new = x3 + (x1 - x3) * p;
+                float y3new = y3 + (y1 - y3) * p;
+
+                Point t1 = new Point(x1, y1);
+                Point t2 = new Point(x2, y2);
+                Point t3 = new Point(x3, y3);
+                Point t1new = new Point((int)x1new, (int)y1new);
+                Point t2new = new Point((int)x2new, (int)y2new);
+                Point t3new = new Point((int)x3new, (int)y3new);
+
+                g.DrawPolygon(mypen, new Point[] { t1, t1new, t3new });
+                g.DrawPolygon(mypen, new Point[] { t1new, t2, t2new });
+                g.DrawPolygon(mypen, new Point[] { t2new, t3, t3new });
+
+                depth--;
+
+                DrawTriangles(x1, y1, (int)x1new, (int)y1new, (int)x3new, (int)y3new, mypen, depth, p);
+                DrawTriangles((int)x1new, (int)y1new, x2, y2, (int)x2new, (int)y2new, mypen, depth, p);
+                DrawTriangles((int)x3new, (int)y3new, (int)x2new, (int)y2new, x3, y3, mypen, depth, p);
+
+            }
+        }
+
     }
 }
